b31590459c3c461f48c48309a8b62f863c7d95be
Graziano Aloisi <graziano.aloisi@gmail.com>
2025-05-25 14:36:01 +0200

    primo commit

---
+++ src/main/java/com/auth/spage/SingleSpringPageApplication.java
@@ -0,0 +1,13 @@
+package com.auth.spage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleSpringPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleSpringPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth/spage/SingleSpringPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.spage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleSpringPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
